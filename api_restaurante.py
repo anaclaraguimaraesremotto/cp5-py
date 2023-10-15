ba282d9402866823acaf5ae630dd18c495e7ac00
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2023-10-14 22:11:19 -0300

    insert

--- api_restaurante.py
+++ api_restaurante.py
@@ -1,21 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-import db_cardapio
+# import db_cardapio
 import traceback
 
 app = Flask(__name__)
+CORS(app)  
 
-CORS(app)
+@app.route("/cardapio", methods=["GET"])
+def get_cardapio():
+    
+    cardapio_data = db_cardapio.get_cardapio()
+    return jsonify(cardapio_data)
 
 @app.route("/cardapio", methods=["POST"])
 def insere_cardapio():
     try:
         dado = request.json
-        db_cardapio.insert_cardapio(dado)
-        return dado, 200
+        db_cardapio.insert_cardapio(dado) 
+        return jsonify({"msg": "Item de cardápio inserido com sucesso"}), 201
     except Exception as erro:
         traceback.print_exc()
-        info = {"msg" : "Erro de sistema"}
-        return info, 406
-    
-app.run(debug=True)
+        return jsonify({"msg": "Erro de sistema"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
